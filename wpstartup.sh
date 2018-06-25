5246cd5d1537f5d8c974e38b97d09fbab0a4d236
jianliyow <jianliyow@users.noreply.github.com>
2018-06-25 15:54:48 +0800

    Update wpstartup.sh

--- wpstartup.sh
+++ wpstartup.sh
@@ -4,7 +4,7 @@ mv wp-content/ temp/
 mv composer.json temp-composer.json
 cp -a wordpress/. .
 rm -rf wp-content
-mv temp/ wp-content/
+mv temp wp-content
 rm -rf wordpress/
 mv wp-config-sample.php wp-config.php
 mv temp-composer.json composer.json
