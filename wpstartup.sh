499880e5c1b33a39b3ab18df7dc041cc0e64f61e
jianoli <jianoli@gmail.com>
2018-10-12 18:19:27 +0800

    WP Engine build from: WP Engine build from: project in path@67c2f1a@1c4d869

--- wpstartup.sh
+++ wpstartup.sh
@@ -1,11 +1,7 @@
 #! /bin/bash
 
 composer install
-mv wp-content/ tmp-wp-content/
+mv wp-content tmp-wp-content
 mv composer.json tmp-composer.json
 mv wp-config.php tmp-wp-config.php
-cp -a wordpress/. .
-mv tmp-wp-content/ wp-content/
-mv tmp-composer.json composer.json
-mv tmp-wp-config.php wp-config.php
-rm -rf wordpress/
+
