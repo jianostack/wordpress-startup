1c4d869027c0987fe8110a403d4e5de7f0a7a770
jianoli <jianoli@gmail.com>
2018-10-12 18:06:19 +0800

    WP Engine build from: project in path@67c2f1a

--- wpstartup.sh
+++ wpstartup.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
+composer install
 mv wp-content/ tmp-wp-content/
 mv composer.json tmp-composer.json
 mv wp-config.php tmp-wp-config.php
 cp -a wordpress/. .
-rm -rf wp-content
 mv tmp-wp-content/ wp-content/
-rm -rf wordpress/
 mv tmp-composer.json composer.json
 mv tmp-wp-config.php wp-config.php
-cd wp-content/themes/superleague 
-composer install
-cd ../../../
+rm -rf wordpress/
