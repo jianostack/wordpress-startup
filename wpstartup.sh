560de02a643f890cbc5cbd420e4c5a145a13d746
jianoli <jianoli@gmail.com>
2018-10-12 18:29:51 +0800

    corrected wpstartup script

--- wpstartup.sh
+++ wpstartup.sh
@@ -4,4 +4,8 @@ composer install
 mv wp-content tmp-wp-content
 mv composer.json tmp-composer.json
 mv wp-config.php tmp-wp-config.php
-
+cp -a wordpress/. .
+rm -rf wp-content
+mv tmp-wp-content wp-content
+mv tmp-composer.json composer.json
+mv tmp-wp-config.php wp-config.php
