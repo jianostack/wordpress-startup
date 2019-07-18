e7db96e6e4ebc77ea6f6234af76afd217700e181
jianliyow <jianli@massiveinfinity.com>
2019-07-18 15:54:46 +0800

    Removed wp-config.php from startup script.

--- wpstartup.sh
+++ wpstartup.sh
@@ -4,7 +4,6 @@ composer install
 npm i
 mv wp-content tmp-wp-content
 mv composer.json tmp-composer.json
-mv wp-config.php tmp-wp-config.php
 cp -a wordpress/. .
 rm -rf wp-content
 mv tmp-wp-content wp-content
